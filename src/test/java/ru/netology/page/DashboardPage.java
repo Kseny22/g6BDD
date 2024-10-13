b42eb47251dcc49ae13b0b2878a69a27465149f2
Emma Paris <misyura.c@yandex.ru>
2024-10-14 01:37:18 +0300

    test

---
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -0,0 +1,46 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import lombok.val;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id='dashboard']");
+    private static ElementsCollection cards = $$(".list__item div");
+    private static final String balanceStart = "баланс: ";
+    private static final String balanceFinish = " р.";
+    private SelenideElement deposit = $("[data-test-id='action-deposit']");
+    private SelenideElement reload = $("[data-test-id='action-reload']");
+
+    public DashboardPage() {
+        heading.shouldBe(Condition.visible);
+    }
+
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+
+    public TransferCardPage selectCard(DataHelper.CardInfo cardInfo) {
+        cards.findBy(Condition.attribute("data-test-id", cardInfo.getCardID()))
+                .$("[data-test-id='action-deposit']").click();
+        return new TransferCardPage();
+    }
+
+    public SelenideElement findCard(DataHelper.CardInfo card) {
+        return cards.findBy(Condition.attribute("data-test-id", card.getCardID()));
+    }
+
+    public int getCardBalance(DataHelper.CardInfo card) {
+        val text = findCard(card).text();
+        return extractBalance(text);
+    }
+}
---
+++ src/test/java/ru/netology/test/MoneyTransferTests.java
@@ -0,0 +1,87 @@
+package ru.netology.test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MoneyTransferTests {
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+
+    @Test
+    public void shouldTransferMoneyFromCardOneToCardTwo() {
+        DashboardPage dashboardPage = new DashboardPage();
+
+        var cardOneInfo = DataHelper.getFirstCard();
+        var cardTwoInfo = DataHelper.getSecondCard();
+        var cardOneBalance = dashboardPage.getCardBalance(cardOneInfo);
+        var cardTwoBalance = dashboardPage.getCardBalance(cardTwoInfo);
+        var sum = DataHelper.getValidSum(cardOneBalance);
+
+        var transfer = dashboardPage.selectCard(cardTwoInfo);
+        transfer.getValidTransfer(String.valueOf(sum), cardOneInfo);
+
+        Assertions.assertEquals(cardOneBalance - sum, dashboardPage.getCardBalance(cardOneInfo));
+        Assertions.assertEquals(cardTwoBalance + sum, dashboardPage.getCardBalance(cardTwoInfo));
+    }
+
+    @Test
+    public void shouldTransferMoneyFromCardTwoToCardOne() {
+        DashboardPage dashboardPage = new DashboardPage();
+
+        var cardOneInfo = DataHelper.getFirstCard();
+        var cardTwoInfo = DataHelper.getSecondCard();
+        var cardOneBalance = dashboardPage.getCardBalance(cardOneInfo);
+        var cardTwoBalance = dashboardPage.getCardBalance(cardTwoInfo);
+        var sum = DataHelper.getValidSum(cardTwoBalance);
+
+        var transfer = dashboardPage.selectCard(cardOneInfo);
+        transfer.getValidTransfer(String.valueOf(sum), cardTwoInfo);
+
+        Assertions.assertEquals(cardTwoBalance - sum, dashboardPage.getCardBalance(cardTwoInfo));
+        Assertions.assertEquals(cardOneBalance + sum, dashboardPage.getCardBalance(cardOneInfo));
+    }
+
+    @Test
+    public void shouldTransferMoneyCancel() {
+        DashboardPage dashboardPage = new DashboardPage();
+
+        var cardOneInfo = DataHelper.getFirstCard();
+        var cardTwoInfo = DataHelper.getSecondCard();
+        var cardOneBalance = dashboardPage.getCardBalance(cardOneInfo);
+        var cardTwoBalance = dashboardPage.getCardBalance(cardTwoInfo);
+
+        var transfer = dashboardPage.selectCard(cardOneInfo);
+        transfer.cancelRefill();
+
+        Assertions.assertEquals(cardTwoBalance, dashboardPage.getCardBalance(cardTwoInfo));
+        Assertions.assertEquals(cardOneBalance, dashboardPage.getCardBalance(cardOneInfo));
+    }
+
+    @Test
+    public void shouldTransferMoneyMoreThanBalance() {
+        DashboardPage dashboardPage = new DashboardPage();
+
+        var cardOneInfo = DataHelper.getFirstCard();
+        var cardTwoInfo = DataHelper.getSecondCard();
+        var cardTwoBalance = dashboardPage.getCardBalance(cardTwoInfo);
+        var sum = DataHelper.getInvalidSum(cardTwoBalance);
+
+        var transfer = dashboardPage.selectCard(cardOneInfo);
+        transfer.refillCard(String.valueOf(sum), cardTwoInfo);
+        transfer.getInvalidTransfer("Ошибка! Недостаточно средств на карте!");
+    }
+}
